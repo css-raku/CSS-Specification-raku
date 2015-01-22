40de978727c91d28ea0b20c66257c96aa3e06585
David Warring david.warring@gmail.com <david.warring@gmail.com>
2015-01-22 12:33:09 -0800

    [t/CSS/Aural/BadGrammar.pm] - more comment tweaks

--- t/CSS/Aural/BadGrammar.pm
+++ t/CSS/Aural/BadGrammar.pm
@@ -7,6 +7,6 @@ grammar CSS::Aural::BadGrammar
     is CSS::Aural::Spec::Grammar 
     is CSS::Grammar::CSS21
     does CSS::Aural::Spec::Interface {
-        # this grammar doesn't provide interface methods - should fail, e.g.:
+        # this grammar doesn't provide interface methods - should fail at compilation, e.g.:
         # "Method 'generic-voice' must be implemented by CSS::Aural::BadGrammar because it is required by a role"
 }
